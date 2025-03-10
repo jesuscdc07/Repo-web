549a62de44554dc974a19bdbfc74c901747ea7c1
jesuscruz <tic-300171@utnay.edu.mx>
2025-03-09 18:43:36 -0700

    cambio welcome-home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <h1>El comienzo de todo</h1>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');   //cambiar welcome por ruta personalizada
 });
+
