525fc4b4e17fae136fe50bb95ba2ed55939c255c
jesuscruz <tic-300171@utnay.edu.mx>
2025-03-10 23:58:55 -0700

    cambios ez

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--evita que la pagina se vea pequeña en disp mov-->
     <title>pagina principal</title>
+    <link rel="stylesheet" href="Home.css">
 </head>
 <body>
-    <header>
-
+    <header class="header">
+        <div class="logo-header">
+             <img src="images/" alt="">
+        </div>
     </header>
     <h1>Pagina de inicio</h1>
 
 
-    
+
     <footer>
 
     </footer>
