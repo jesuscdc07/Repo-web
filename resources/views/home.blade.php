9106e43ce7aff80ca9a3221008a7d6f103be4464
jesuscruz <tic-300171@utnay.edu.mx>
2025-03-10 23:45:52 -0700

    agregue hoja de estilos para index principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--evita que la pagina se vea pequeña en disp mov-->
     <title>pagina principal</title>
 </head>
 <body>
@@ -11,6 +11,8 @@
     </header>
     <h1>Pagina de inicio</h1>
 
+
+    
     <footer>
 
     </footer>
